ea61a7d84486d5c27721c55d0181d00aea64371c
姚超臣 <yaochaochen@ctv.org.cn>
2019-10-29 22:39:07 +0800

    java deep stream is group Map

---
+++ src/com/yuetu/deep/in/java/collection/StreamDemo.java
@@ -0,0 +1,22 @@
+package com.yuetu.deep.in.java.collection;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamDemo {
+
+    public static void main(String[] args) {
+
+        List<RtdbData> lists = new ArrayList<>();
+        lists.add(new RtdbData("A","1"));
+        lists.add(new RtdbData("B", "1"));
+        lists.add(new RtdbData("A" , "0"));
+        lists.add(new RtdbData("B", "0"));
+        Map<String, List<RtdbData>> map = lists.stream().collect(Collectors.groupingBy(RtdbData::getValues));
+        System.out.println(map.size());
+    }
+
+}
