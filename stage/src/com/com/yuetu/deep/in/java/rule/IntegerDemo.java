18a3dde729720c1c837682ece09e4a1fab794bb0
姚超臣 <yaochaochen@ctv.org.cn>
2019-12-03 14:56:50 +0800

    Integer 缓存

---
+++ stage/src/com/com/yuetu/deep/in/java/rule/IntegerDemo.java
@@ -0,0 +1,12 @@
+package com.yuetu.deep.in.java.rule;
+
+public class IntegerDemo {
+
+    public static void main(String[] args) {
+
+        Integer a = 100, b = 100, c = 150, d = 150;
+        System.out.println(a == b);
+        System.out.println(c == d);
+
+    }
+}
